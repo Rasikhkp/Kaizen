98201dd3735b92e3000e28d7fb7147cd7fa68abe
rasikhkp <raxiest@gmail.com>
2024-04-03 15:04:21 +0700

    fix: Delete published from state manager

--- redux/store.ts
+++ redux/store.ts
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
 import draftReducer from './features/draft-slice'
-import publishedReducer from './features/published-slice'
 import activeReducer from './features/active-slice'
 
 export const store = configureStore({
     reducer: {
         draft: draftReducer,
-        published: publishedReducer,
         active: activeReducer
     }
 })
@@ -14,5 +12,4 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export const getAllDraft = (state: RootState) => state.draft.values
-export const getAllPublished = (state: RootState) => state.published.values
 export const getActive = (state: RootState) => state.active.values
